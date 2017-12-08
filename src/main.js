63d6567abe9d9967fea8531f5c72cb6946e22cff
vasiliskarlis <vasiliskarlis97@gmail.com>
2017-12-08 14:45:28 +0200

    epistrefei oloklhro to json-den pairnei input akoma

--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
 
 let test =  'hellohellohellohello' ;
+console.log("paixtara mou");
+
+    var fetch = require('node-fetch');
+
+    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e7cd55')
+    .then(function(res) {
+        return res.text();
+        //console.log(res.Title);
+    }).then(function(body) {
+        console.log(body);
+
+
+        });
+
+
+  /*
+  fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e7cd55')
+  .then(function(res) {
+      return res.text();
+  }).then(data =>{
+    var mydata = JSON.parse(data);
+    alert(mydata.Title)
+  }
+   );
+*/
