e55b90a24b798e86c46c3301d92c7f10424b7b40
vasiliskarlis <vasiliskarlis97@gmail.com>
2017-12-10 20:13:25 +0200

    dexetai eisodo emfanizei zhtoumena

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
+var program = require('commander');
+var fetch = require('node-fetch');
 
-let test =  'hellohellohellohello' ;
-console.log("paixtara mou");
+program
+  .version('0.1.0')
+  .option('--imdbID [id]', 'Add the specified movie id [ouaou]', 'ouaou')
+  .parse(process.argv);
+let y = 'http://www.omdbapi.com/?i=' + program.imdbID + '&apikey=8e7cd55'
 
-    var fetch = require('node-fetch');
-
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e7cd55')
+fetch(y)
     .then(function(res) {
-        return res.text();
-        //console.log(res.Title);
-    }).then(function(body) {
-        console.log(body);
+        return res.json();
+    }).then(function(json) {
+        console.log('Title: ' + json.Title);
+        console.log('Year: ' + json.Year);
+        console.log('Rated: ' + json.Rated);
+        console.log('Released: ' + json.Released);
+        console.log('Runtime: ' + json.Runtime);
 
+    });
 
-        });
-
-
-  /*
-  fetch('http://www.omdbapi.com/?i=tt3896198&apikey=8e7cd55')
-  .then(function(res) {
-      return res.text();
-  }).then(data =>{
-    var mydata = JSON.parse(data);
-    alert(mydata.Title)
-  }
-   );
+/* den exw valei afto akoma:Note	that	you	have	to	check	if	the	user	submits	an	id	argument
+otherwise	log	a	message	to	the	user.
 */
